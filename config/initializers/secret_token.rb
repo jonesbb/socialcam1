b1948cb8adec7f06380a7b8e09e84744e2094238
jonesbb <jonesbb@gmail.com>
2012-04-21 13:50:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialcam1::Application.config.secret_token = '922f67f05363d00a2fc10025d99bcc6cb22fc0e3478a8c73652e81c8c0229c4be4ac7486b1d697f9f39962f9c4a759b8db1bcab582e6e57e60d3b4b04daab98a'
